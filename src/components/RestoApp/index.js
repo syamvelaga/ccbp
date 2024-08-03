5264f672bc3c5b96883e770355d1934d3db7aa1f
JaiKumar1003 <jaijai100320@gmail.com>
2024-08-03 11:33:36 +0000

    Solution

--- src/components/RestoApp/index.js
+++ src/components/RestoApp/index.js
@@ -1,26 +1,31 @@
 import {useState, useEffect, useContext} from 'react'
 
+import Loader from 'react-loader-spinner'
+
 import Header from '../Header'
 import Categories from '../Categories'
 import CurrentDishesList from '../CurrentDishesList'
 import MyContext from '../../context/MyContext'
 import './index.css'
 
+const apiStatus = {
+  success: 'SUCCESS',
+  failure: 'FAILURE',
+  loader: 'LOADER',
+}
+
 const RestoApp = () => {
   const {updateRestaurantName} = useContext(MyContext)
+  const [currentApiStatus, setCurrentApiStatus] = useState(apiStatus.loader)
   const [data, setData] = useState([])
-  const [loading, setLoading] = useState(true)
-  const [error, setError] = useState('')
 
   useEffect(() => {
     const fetchData = async () => {
       const url =
         'https://apis2.ccbp.in/restaurant-app/restaurant-menu-list-details'
-      try {
-        const response = await fetch(url)
-        if (!response.ok) {
-          throw new Error('Data not found')
-        }
+
+      const response = await fetch(url)
+      if (response.ok) {
         const responseData = await response.json()
         const restaurantName = responseData[0].restaurant_name
         updateRestaurantName(restaurantName)
@@ -33,28 +38,56 @@ const RestoApp = () => {
           nexturl: eachItem.nexturl,
         }))
         setData(tableMenuList)
-      } catch (err) {
-        setError(err.message)
-      } finally {
-        setLoading(false)
+        setCurrentApiStatus(apiStatus.success)
+      } else {
+        setData([])
+        setCurrentApiStatus(apiStatus.failure)
       }
     }
+
     fetchData()
-  }, [])
+  }, [currentApiStatus, updateRestaurantName])
 
-  if (loading) {
-    return <div className="loading-error-card">Loading...</div>
-  }
-
-  if (error) {
-    return <div className="loading-error-card">Error: {error}</div>
-  }
-
-  return (
+  const renderSuccessView = () => (
     <div className="resto-container">
       <Header />
       <Categories reptoDishes={data} />
       <CurrentDishesList />
+    </div>
+  )
+
+  const onClickRetry = () => {
+    setCurrentApiStatus(apiStatus.loader)
+  }
+
+  const renderFailureView = () => (
+    <div className="common-container">
+      <p className="not-found-heading">Not Found</p>
+      <button onClick={onClickRetry} type="button" className="retry-button">
+        Retry
+      </button>
+    </div>
+  )
+
+  const renderApiStatus = () => {
+    if (currentApiStatus === apiStatus.success) {
+      return renderSuccessView()
+    }
+
+    return renderFailureView()
+  }
+
+  const renderLoader = () => (
+    <div className="common-container">
+      <Loader type="ThreeDots" height={80} width={80} color="black" />
+    </div>
+  )
+
+  return (
+    <div>
+      {currentApiStatus === apiStatus.loader
+        ? renderLoader()
+        : renderApiStatus()}
     </div>
   )
 }
